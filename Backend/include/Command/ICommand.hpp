9f3d7dfb7bbcde74beb75054678fb8de22ebe9f0
WorHyako <worhyako@gmail.com>
2024-09-05 10:42:09 +0200

    java doc. part 1

--- Backend/include/Command/ICommand.hpp
+++ Backend/include/Command/ICommand.hpp
@@ -26,9 +26,9 @@ namespace Mss::Backend::Command {
 		 *
 		 * @param	sessionName	Session name to send command.
 		 *
-		 * @return	<code>true</code> success executing
+		 * @return	@code true @endcode		Success executing
 		 *			<p>
-		 *			<code>false</code> fail in executing
+		 *			@code false @endcode	Fail in executing
 		 */
 		[[nodiscard]]
 		virtual bool execute(std::string sessionName) const noexcept = 0;
@@ -41,35 +41,35 @@ namespace Mss::Backend::Command {
 		/**
 		 * @brief   Add selected item to command.
 		 *
-		 * @param   item Item to add
+		 * @param   item	Item to add
 		 */
 		virtual void addItem(CommandItem item) noexcept = 0;
 
 		/**
 		 * @brief   Add selected items to command.
 		 *
-		 * @param   items Items to add.
+		 * @param   items	Items to add.
 		 */
 		virtual void addItems(std::vector<CommandItem> items) noexcept = 0;
 
 		/**
 		 * @brief   Removes item by key from command
 		 *
-		 * @param   item Item to remove
+		 * @param   item	Item to remove
 		 */
 		virtual void removeItem(CommandItem item) noexcept = 0;
 
 		/**
 		 * @brief   Removes item by key from command.
 		 *
-		 * @param   key Key of item to remove
+		 * @param   key		Key of item to remove
 		 */
 		virtual void removeItem(std::string key) noexcept = 0;
 
 		/**
 		 * @brief   Removes item by index from command.
 		 *
-		 * @param   idx Index of item to remove.
+		 * @param   idx		Index of item to remove.
 		 */
 		virtual void removeItem(std::uint16_t idx) noexcept = 0;
 
@@ -83,7 +83,7 @@ namespace Mss::Backend::Command {
 		 *          <p>
 		 *          Nothing will happen, if item doesn't exist.
 		 *
-		 * @param   item Item to change
+		 * @param   item	Item to change
 		 */
 		virtual void changeItem(CommandItem item) noexcept = 0;
 
@@ -101,11 +101,11 @@ namespace Mss::Backend::Command {
 		/**
 		 * @brief   Returns index of item with selected key.
 		 *
-		 * @param   key Item's key to search.
+		 * @param   key		Item's key to search.
 		 *
 		 * @return  Index of item
 		 *          <p/>
-		 *          <code>-1</code> if there is no item with selected key
+		 *          @code -1 @endcode If there is no item with selected key
 		 */
 		[[nodiscard]]
 		virtual std::int16_t indexOf(std::string key) const noexcept = 0;
@@ -132,7 +132,7 @@ namespace Mss::Backend::Command {
 		/**
 		 * @brief   Marks command with selected tag.
 		 *
-		 * @param   tag Command tag
+		 * @param   tag		Command tag
 		 */
 		virtual void tag(std::string tag) noexcept = 0;
 
@@ -147,7 +147,7 @@ namespace Mss::Backend::Command {
 		/**
 		 * @brief   Try to parse selected string to command items and fill it.
 		 *
-		 * @param   commandStr Command via string
+		 * @param   commandStr	Command via string
 		 */
 		virtual void set(const std::string &commandStr) noexcept = 0;
 
--- Backend/include/Midi/MidiRoadMap.hpp
+++ Backend/include/Midi/MidiRoadMap.hpp
@@ -9,7 +9,16 @@
 
 namespace Mss::Backend::Midi {
 	/**
-	 * @brief
+	 * @brief	Object to store and control all roads.
+	 *			<p>
+	 *			Recommend to use only as Singleton object.
+	 *
+	 * @usage
+	 * @code
+	 *			auto &roadMap = Wor::Wrappers::Singleton<Backend::Midi::MidiRoadMap>::get();
+	 *			roadMap.load();
+	 *			auto road = roadMap.midiRoad(midiButtonIndex);
+	 * @endcode
 	 *
 	 * @author WorHyako
 	 */
@@ -26,81 +35,108 @@ namespace Mss::Backend::Midi {
 		virtual ~MidiRoadMap() noexcept = default;
 
 		/**
-		 * @brief
+		 * @brief	Adds new road or overwrite existing one.
 		 *
-		 * @param midiKeyIdx
+		 * @param	midiKeyIdx	Midi button id. It will be uses as road's index.
 		 *
-		 * @param road
+		 * @param	road		New road
 		 */
 		void addRoad(std::uint8_t midiKeyIdx, MidiRoad road) noexcept;
 
 		/**
-		 * @brief
+		 * @brief	Adds new road or overwrite existing one.
+		*
+		 * @param	midiKeyIdx	Midi button id. It will be uses as road's index.
 		 *
-		 * @param midiKeyIdx
-		 *
-		 * @param road
+		 * @param	road		New road
 		 */
 		void addRoad(std::uint8_t midiKeyIdx, ServerRoad road) noexcept;
 
 		/**
-		 * @brief
+		 * @brief	Transmits incoming callbacks to road map and transfer in to roads.
 		 *
-		 * @param callbackInfo
+		 * @usage
+		 * @code
+		 * 			auto &roadMap = Wrappers::Singleton<Mss::Backend::Midi::MidiRoadMap>::get();
+		 *			midi.inCallback([&roadMap](Midi::CallbackInfo::BaseCallbackInfo callbackInfo) {
+		 *				roadMap.transmit(callbackInfo);
+		 *			});
+		 * @endcode
+		 *
+		 * @param	callbackInfo
 		 */
 		void transmit(const Wor::Midi::CallbackInfo::BaseCallbackInfo &callbackInfo) noexcept;
 
 		/**
-		 * @brief
+		 * @brief	Delete all roads from storage.
 		 */
 		void clear() noexcept;
 
 		/**
-		 * @brief
+		 * @brief	Remove midi and server road by index.
 		 *
-		 * @param midiKeyIdx
+		 * @param	midiKeyIdx Road index.
 		 */
 		void removeRoad(std::uint8_t midiKeyIdx) noexcept;
 
 		/**
-		 * @brief
+		 * @brief	Save current midi and server roads to json file.
+		 *			<p>
+		 *			File path - "Resources/RoadMap/midi.json"
 		 */
 		void save() const noexcept;
 
 		/**
-		 * @brief
+		 * @brief	Load midi and server roads from json file.
+		 *			<p>
+		 *			File path - "Resources/RoadMap/midi.json"
 		 */
 		void load() noexcept;
 
 	private:
+		/**
+		 * @brief Stores server roads. Midi button index uses as key.
+		 */
 		std::unordered_map<std::uint8_t, ServerRoad> _serverRoad;
 
+		/**
+		 * @brief Stores midi roads. Midi button index uses as key.
+		 */
 		std::unordered_map<std::uint8_t, MidiRoad> _midiRoad;
 
+		/**
+		 * @brief	Road id corresponds to midi button id.
+		 *			<p>
+		 *			Uses as index of road maps.
+		 */
 		std::uint8_t _buttonIdIdx;
 
 	public:
 #pragma region Accessors/Mutators
 
 		/**
-		 * @brief
+		 * @brief	Midi button id accessor.
 		 *
-		 * @param idx
+		 * @param	idx Midi button id.
 		 */
 		void buttonIdIdx(std::uint8_t idx) noexcept;
 
 		/**
-		 * @brief
+		 * @brief	Midi road accessor.
 		 *
-		 * @return
+		 * @param	midiIdx	Midi button id.
+		 *
+		 * @return	Midi road if it's exists.
 		 */
 		[[nodiscard]]
 		std::optional<MidiRoad> midiRoad(std::uint8_t midiIdx) const noexcept;
 
 		/**
-		 * @brief
+		 * @brief	Server road accessor.
 		 *
-		 * @return
+		 * @param	midiIdx	Midi button id.
+		 *
+		 * @return	Server road if it's exists.
 		 */
 		[[nodiscard]]
 		std::optional<ServerRoad> serverRoad(std::uint8_t midiIdx) const noexcept;
